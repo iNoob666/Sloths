fc68a0296f6636ca36e2b143211816ee3da51ace
Max <vfrcbv180999@gmail.com>
2021-03-07 01:50:55 +0600

    min iFigure iFabric

--- source/math/IFigure.cs
+++ source/math/IFigure.cs
@@ -4,10 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace Sloths.source.math
 {
     public interface IFigure
     {
+        bool IsIn(Point p);         //принадлежность точки фигуре
+        IFigure Scale(double percentX, double percentY);        //изменение размера фигуры процентами
+        IFigure Fit(double w, double h);        //изменение размера фигуры значениями
+        IFigure MoveByVector(Point v);          //перемещение
+        IFigure Rotate(Point center, double Phi);         //поворот 
+        IFigure Rotate(double Phi);         //поворот относительно центра
+        IFigure Reflection(Point a, Point b);   //отражение
+        void Draw(IGraphic screen, IFigureGraphicParameters param); //рисование
     }
 }
--- source/model/IFabric.cs
+++ source/model/IFabric.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sloths.source.math
 
 namespace Sloths.source.model
 {
     public interface IFabric
     {
-
+        IFigure create();
     }
 }
